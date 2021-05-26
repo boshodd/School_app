066a8e5caa88632683d025772f3cb7988fcf8dab
Bosco Kuttikatt <66684265+boshodd@users.noreply.github.com>
2021-05-26 11:40:14 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/school_app/MainActivity.java
+++ app/src/main/java/com/example/school_app/MainActivity.java
@@ -17,7 +17,17 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private Button lb;
@@ -25,23 +35,94 @@ public class MainActivity extends AppCompatActivity {
     private MyDbHandler db;
     SharedPreferences sp;
 
+    private String username1;
+    private String password1;
+    //private String URL="http://10.0.2.2/loginregister/login.php";
+    private String URL="http://schoolplusplus.000webhostapp.com/php_files/newlogin.php";
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         db= new MyDbHandler(MainActivity.this);
-        t1= findViewById(R.id.username);
-        t2= findViewById(R.id.password);
+        /*t1= findViewById(R.id.username);
+        t2= findViewById(R.id.password);*/
         lb= findViewById(R.id.loginbtn);
         sp=getSharedPreferences("MyUserPrefs", Context.MODE_PRIVATE);
 
+        sp = getApplicationContext().getSharedPreferences("MyUserPrefs", Context.MODE_PRIVATE);
+        final String sp_db = sp.getString("selectdb", "");
+
+        username1 = password1 = "";
+        t1= findViewById(R.id.username);
+        t2= findViewById(R.id.password);
 
        lb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String s1=t1.getText().toString();
+                Toast.makeText(MainActivity.this, "Loading", Toast.LENGTH_SHORT).show();
+                username1 =t1.getText().toString().trim();
+                password1 =t2.getText().toString().trim();
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString("Username",username1);
+                editor.commit();
+                final String sp_db = sp.getString("selectdb", "");
+
+                if(!username1.equals("") && !password1.equals("")){
+                        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                            if (response.equals("Success")) {
+                                Intent intent = new Intent(MainActivity.this, Dashboard.class);
+                                startActivity(intent);
+                                finish();
+                            } else if (response.equals("Failure")) {
+                                Toast.makeText(MainActivity.this, "Invalid Login Id/Password", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(MainActivity.this, "Bosco error is "+error.toString().trim(), Toast.LENGTH_SHORT).show();
+                            }
+                        }){
+                            @Override
+                            public Map<String, String> getParams() throws AuthFailureError {
+                                Map<String, String> data=new HashMap<>();
+                                data.put("t1", username1);
+                                data.put("t2", password1);
+                                data.put("tablename",sp_db);
+                                return data;
+                            }
+                        };
+                        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+                        requestQueue.add(stringRequest);
+                    }
+                else{
+                    Toast.makeText(MainActivity.this, "All Fields are mandatory", Toast.LENGTH_SHORT).show();
+                }
+
+                }
+            });
+
+    }
+
+    public void signupfunc(View view){
+        Intent intent=new Intent(this,Sign_up.class);
+        startActivity(intent);
+    }
+
+    public void toForgotPass(View view) {
+        Intent intent=new Intent(this,ForgotPass.class);
+        startActivity(intent);
+    }
+}
+
+ /*String s1=t1.getText().toString();
                 String s2=t2.getText().toString();
                 Boolean chkmailpass=db.emailpassword(s1,s2);
                 if((s1.equals("")) || (s2.equals(""))){
@@ -61,19 +142,4 @@ public class MainActivity extends AppCompatActivity {
                     }
                     else Toast.makeText(MainActivity.this, "Wrong Credentials", Toast.LENGTH_SHORT).show();
 
-                }
-
-
-            }
-        });
-
-
-    }
-
-    public void signupfunc(View view){
-        Intent intent=new Intent(this,Sign_up.class);
-        startActivity(intent);
-
-
-    }
-}
+                }*/
